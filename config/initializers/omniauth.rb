c2c09f6a25b0fd6a8ba8c2910ca9ba08ecc002db
wiseosagie <wiseosagie@yahoo.com>
2017-01-21 14:10:47 -0800

    adjust the authentication with facebook 2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,6 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :facebook, ENV['1062834280493751'], ENV['7fd61b735c02a2b2c694330977399dfe'], scope: "email, publish_stream"
 
-  provider :facebook, '1062834280493751', '7fd61b735c02a2b2c694330977399dfe', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, '1062834280493751', '7fd61b735c02a2b2c694330977399dfe', provider_ignores_states: true
+  # {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
