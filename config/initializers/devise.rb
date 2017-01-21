a343e4f99a0f0202cdd94ab959d94173268585f4
wiseosagie <wiseosagie@yahoo.com>
2017-01-21 15:19:26 -0800

    adjust the authentication with facebook 4

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,8 +248,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-   config.omniauth :facebook, ENV['1062834280493751'], ENV['7fd61b735c02a2b2c694330977399dfe'], provider_ignores_states: true
-
+   config.omniauth :facebook, ENV['1062834280493751'], ENV['7fd61b735c02a2b2c694330977399dfe']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,9 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :facebook, ENV['1062834280493751'], ENV['7fd61b735c02a2b2c694330977399dfe'], scope: "email, publish_stream"
 
-  provider :facebook, '1062834280493751', '7fd61b735c02a2b2c694330977399dfe', provider_ignores_states: true
-  # {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, '1062834280493751', '7fd61b735c02a2b2c694330977399dfe', {:scope => 'PERMISSION_1, PERMISSION_2, PERMISSION_3...', :client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  # provider :facebook, 'YOUR_APP_ID', 'YOUR_SECRET_KEY',
+  #          {:scope => 'PERMISSION_1, PERMISSION_2, PERMISSION_3...', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+
+
 end
