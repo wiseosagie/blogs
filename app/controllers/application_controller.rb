4fadbd2cf32eb2567af0b4c7fe53fc1b14bd84da
wiseosagie <wiseosagie@yahoo.com>
2017-01-23 20:59:57 -0800

    adjust the authentication with facebook before filter added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
-  # before_filter :authenticate_user!
+   before_filter :authenticate_user!
 
 
 
